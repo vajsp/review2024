668737e82a9dfd91cf19c474547b242e3f73a506
ma <442026460@qq.com>
2024-06-25 14:11:13 +0800

    bc

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ function a() {
 }
 
 a();
+
+sessionStorage.setItem('key', 'safda');
+
+const formData = [];
+let index = 0;
+let taskPool = [];
+
+while (index < formData.length) {
+    const task = fetch('..//sdafas', {
+        method: 'POST',
+        body: formData[index],
+    });
+
+    task.then(() => {
+        const index = taskPool.findIndex((item) => {
+            return item === task;
+        });
+        taskPool.splice(index, 1);
+    });
+
+    taskPool.push(task);
+    index++;
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
+type mReadonly<T> = {
+    readonly [P in keyof T]: T[P];
+};
 
+type mPick<T, K extends keyof T> = {
+    [P in K]: T[P];
+};
+
+type mRecord<K extends keyof any, T> = {
+    [P in K]: T;
+};
+
+interface IDog {
+    name: string;
+    age: number;
+    height: number;
+    weight: number;
+    sex: string;
+}
+
+type mOmit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
